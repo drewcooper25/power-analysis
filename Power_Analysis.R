849c18184274c3aa6fcf722329b49e873f19a0f4
Drew Cooper <user@DMC-MBP.fritz.box>
2020-11-18 01:56:26 +0100

    Non-parametric update

--- Power_Analysis.R
+++ Power_Analysis.R
@@ -1,17 +1,27 @@
 # OPEN Power Analysis!
 
-# https://www.statmethods.net/stats/power.html
+# Normally Distributed Data
 library("pwr")
 
 # using most recent HbA1c (% DCCT) as a metric...
-# n = population size, m = mean, s = standard deviation
-# group 1 = DIY, group 2 = non-DIY
+# x = DIY, y = non-DIY
+nx = 317; mx = 6.22; sdx = 0.99
+ny = 78; my = 6.67; sdy = 1.05
 
-n1 = x; m1 = y; s1 = z
-n2 = x; m2 = y; s2 = z
-
-d = ((abs(m1-m2))/(sqrt((s1+s2)/2)))
-pwr.t2n.test(n1,n2,d,0.05,NULL,alternative=c("two.sided"))
+d = ((abs(mx-my))/(sqrt((sdx+sdy)/2)))
 # can use "two.sided" or "greater" or "less"
+pwr.t2n.test(nx,ny,d,0.05,NULL,alternative=c("greater"))
 
-# N's from above are smaller than total counted individuals from enrollment types; not everyone answers w/ HbA1c values...
+# Non-normally distributed data
+library(MKpower)
+
+nx = 317; ny = 78;
+rx <- rnorm
+ry <- function(x) rnorm(x, mean = 6.67)
+
+# MKpower::
+sim.power.wilcox.test(nx, rx, rx.H0 = NULL, ny, ry, ry.H0 = NULL,  alternative = c("two.sided"), sig.level = 0.05, conf.int = FALSE, approximate = FALSE, ties = FALSE, iter = 10000, nresample = 10000, parallel = "no", ncpus = 1L, cl = NULL)
+
+# minx = 4.8; maxx = 14.9; # for seq()?
+# miny = 4.5; maxy = 12.2; # for seq()?
+# rnorm(n=100, mean=6.22, sd=0.99) // rnorm(n=100, mean=6.67, sd=1.05)
